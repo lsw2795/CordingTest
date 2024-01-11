45781646bf56a3e847322ba173770cff81921ed5
lsw2795 <134687373+lsw2795@users.noreply.github.com>
2024-01-11 17:45:33 +0900

    [level Lv.0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/Lv.0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int num1, int num2) {
         int answer = -1;
         if(num1 == num2){
-            return 1;
+            answer = 1;
         }
         
         return answer;

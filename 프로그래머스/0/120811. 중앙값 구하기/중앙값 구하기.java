d40c202b1c346c87c02047c541a49ddb866e30b8
lsw2795 <134687373+lsw2795@users.noreply.github.com>
2024-01-17 19:09:30 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.33 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        int middleIndex = array.length / 2;
+        answer = array[middleIndex];
+        return answer;
+    }
+}

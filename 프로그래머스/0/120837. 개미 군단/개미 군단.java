efea0a4ff8aee16a39806dd3b559c297917e1f95
lsw2795 <134687373+lsw2795@users.noreply.github.com>
2024-01-24 10:35:48 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        int a = hp / 5;
+        int b = (hp % 5) / 3;
+        int c = (hp % 5) % 3;
+        
+        answer = a + b + c;
+        
+        return answer;
+    }
+}

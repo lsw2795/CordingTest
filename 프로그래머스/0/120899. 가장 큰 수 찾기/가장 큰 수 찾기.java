81821afa41ff52a6753da559104f9ff4da7b3338
lsw2795 <134687373+lsw2795@users.noreply.github.com>
2024-01-25 22:13:33 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = {};
+        answer = new int[2];
+        int max = array[0];
+        int index = 0;
+        
+        for(int i = 1; i < array.length; i++){
+            if(array[i] > max){
+                max = array[i];
+                index = i;
+            }
+        }
+        
+        answer[0] = max;
+        answer[1] = index;
+        
+        return answer;
+    }
+}
